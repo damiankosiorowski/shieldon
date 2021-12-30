1eb842d258d1eb63b041daace1043a3709b898b8
damiankosiorowski <damian.kosiorowski@gmail.com>
2021-12-30 13:17:07 +0100

    Adds support for exclusion sets of query params - template

--- templates/panel/exclusion.php
+++ templates/panel/exclusion.php
@@ -6,9 +6,9 @@
  *
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
- * 
+ *
  * php version 7.1.0
- * 
+ *
  * @category  Web-security
  * @package   Shieldon
  * @author    Terry Lin <contact@terryl.in>
@@ -31,7 +31,7 @@ $timezone = '';
 <div class="so-dashboard">
     <div id="so-rule-table-form" class="so-datatables">
         <div class="so-datatable-heading">
-            <?php _e('panel', 'excl_heading', 'Exclusion'); ?>
+            <?php _e('panel', 'excl_heading_urls', 'Exclusion URLs'); ?>
             <br />
         </div>
         <div class="so-datatable-description">
@@ -65,7 +65,7 @@ $timezone = '';
                         <?php _e('panel', 'ipma_text_nodata', 'No data is available now.'); ?>
                     </td>
                 </tr>
-            </tbdoy>
+            </tbody>
         </table>
     </div>
     <?php else: ?>
@@ -95,9 +95,81 @@ $timezone = '';
                 </tr>
                 <?php endforeach; ?>
                 <?php endif; ?>
-            </tbody>   
+            </tbody>
         </table>
     </div>
+	<br />
+</div>
+<div class="so-qp-dashboard">
+	<div id="so-rule-table-form" class="so-datatables">
+		<div class="so-datatable-heading">
+			<?php _e('panel', 'excl_heading_query_params', 'Exclusion query params'); ?>
+			<br />
+		</div>
+		<div class="so-datatable-description">
+			<?php _e('panel', 'excl_description_query_params', 'Please enter comma-separated name of query params you want them excluded from Shieldon protection.'); ?>
+			<br />
+		</div>
+		<div class="so-rule-form iptables-form">
+			<form method="post" onsubmit="freezeUI();">
+				<div class="d-inline-block align-top">
+					<label for="query-params"><?php _e('panel', 'auth_label_query_params', 'Set of query params names'); ?></label><br />
+					<input name="query_params" id="query-params" type="text" value="" class="regular-text">
+					<span class="form-text text-muted">e.g. <code>abc,redirect</code></span>
+				</div>
+				<div class="d-inline-block align-top">
+					<label>&nbsp;</label><br />
+					<?php echo $this->fieldCsrf(); ?>
+					<input type="hidden" name="action" value="add-qp">
+					<input type="hidden" name="order" value="">
+					<input type="submit" name="submit" id="btn-add-rule-qp" class="button button-primary" value="<?php _e('panel', 'auth_btn_submit', 'Submit'); ?>">
+				</div>
+			</form>
+		</div>
+	</div>
+	<br />
+	<?php if (empty($exclusion_query_params_list)) : ?>
+		<div id="so-qp-table-container" class="so-datatables">
+			<table id="so-qp-datalog" class="cell-border compact stripe responsive" cellspacing="0" width="100%">
+				<tbody>
+				<tr>
+					<td>
+						<?php _e('panel', 'ipma_text_nodata', 'No data is available now.'); ?>
+					</td>
+				</tr>
+				</tbody>
+			</table>
+		</div>
+	<?php else: ?>
+		<div id="so-qp-table-loading" class="so-datatables">
+			<div class="lds-css ng-scope">
+				<div class="lds-ripple">
+					<div></div>
+					<div></div>
+				</div>
+			</div>
+		</div>
+	<?php endif; ?>
+	<div id="so-qp-table-container" class="so-datatables" style="display: none;">
+		<table id="so-qp-datalog" class="cell-border compact stripe responsive" cellspacing="0" width="100%">
+			<thead>
+			<tr>
+				<th><?php _e('panel', 'auth_label_query_params', 'Sets of params'); ?></th>
+				<th><?php _e('panel', 'auth_label_remove', 'Remove'); ?></th>
+			</tr>
+			</thead>
+			<tbody>
+			<?php if (!empty($exclusion_query_params_list)) : ?>
+				<?php foreach ($exclusion_query_params_list as $i => $params) : ?>
+					<tr>
+						<td><?php echo implode(', ', $params); ?></td>
+						<td><button type="button" class="button btn-remove-ip" data-order="<?php echo $i; ?>"><i class="far fa-trash-alt"></i></button></td>
+					</tr>
+				<?php endforeach; ?>
+			<?php endif; ?>
+			</tbody>
+		</table>
+	</div>
 </div>
 
 <script>
@@ -105,7 +177,7 @@ $timezone = '';
     $(function() {
         $('#so-datalog').DataTable({
             'responsive': true,
-            'pageLength': 25,
+            'paging': false,
             'initComplete': function(settings, json) {
                 $('#so-table-loading').hide();
                 $('#so-table-container').fadeOut(800);
@@ -113,13 +185,31 @@ $timezone = '';
             }
         });
 
+		$('#so-qp-datalog').DataTable({
+			'responsive': true,
+			'paging': false,
+			'initComplete': function(settings, json) {
+				$('#so-qp-table-loading').hide();
+				$('#so-qp-table-container').fadeOut(800);
+				$('#so-qp-table-container').fadeIn(800);
+			}
+		});
+
         $('.so-dashboard').on('click', '.btn-remove-ip', function() {
             var order = $(this).attr('data-order');
 
-            $('[name=order]').val(order);
-            $('[name=action]').val('remove');
+            $('.so-dashboard [name=order]').val(order);
+            $('.so-dashboard [name=action]').val('remove');
             $('#btn-add-rule').trigger('click');
         });
+
+		$('.so-qp-dashboard').on('click', '.btn-remove-ip', function() {
+			var order = $(this).attr('data-order');
+
+			$('.so-qp-dashboard [name=order]').val(order);
+			$('.so-qp-dashboard [name=action]').val('remove-qp');
+			$('#btn-add-rule-qp').trigger('click');
+		});
     });
 
 </script>
